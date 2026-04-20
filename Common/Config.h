13d0a7fc1e55546a79317494f52d6ab63e99b1dd
夏绍轩 <xiashaoxuan@xiashaoxuandeMacBook-Pro.local>
2026-04-20 19:23:57 +0800

    init: project skeleton with protocol, config, and 7-week dev plan

---
+++ Common/Config.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "Protocol.h"
+
+namespace TankConfig {
+    struct Attrs {
+        int   maxHP;
+        float speed;
+        float rotateSpeed;
+        int   damage;
+        float bulletSpeed;
+        float skillCooldown;
+        float skillDuration;
+    };
+
+    constexpr Attrs HEAVY = {
+        .maxHP        = 200,
+        .speed        = 1.5f,
+        .rotateSpeed  = 1.5f,
+        .damage       = 30,
+        .bulletSpeed  = 5.0f,
+        .skillCooldown = 15.0f,
+        .skillDuration = 3.0f,
+    };
+
+    constexpr Attrs LIGHT = {
+        .maxHP        = 150,
+        .speed        = 2.5f,
+        .rotateSpeed  = 2.5f,
+        .damage       = 20,
+        .bulletSpeed  = 7.0f,
+        .skillCooldown = 12.0f,
+        .skillDuration = 2.0f,
+    };
+
+    constexpr Attrs SCOUT = {
+        .maxHP        = 100,
+        .speed        = 3.5f,
+        .rotateSpeed  = 3.5f,
+        .damage       = 15,
+        .bulletSpeed  = 9.0f,
+        .skillCooldown = 10.0f,
+        .skillDuration = 0.0f,
+    };
+}
+
+namespace MapConfig {
+    constexpr int WIDTH  = 1200;
+    constexpr int HEIGHT = 800;
+}
+
+namespace NetConfig {
+    constexpr int RECV_BUF_SIZE = 4096;
+    constexpr int SEND_BUF_SIZE = 4096;
+}
---
+++ Common/Protocol.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <cstdint>
+
+enum class MsgID : uint16_t {
+    C2S_LOGIN        = 0x0100,
+    C2S_REGISTER     = 0x0101,
+    S2C_LOGIN_ACK    = 0x0102,
+    S2C_REGISTER_ACK = 0x0103,
+
+    C2S_JOIN_ROOM    = 0x0200,
+    C2S_LEAVE_ROOM   = 0x0201,
+    S2C_ROOM_INFO    = 0x0202,
+    S2C_MATCH_RESULT = 0x0203,
+
+    C2S_INPUT        = 0x0300,
+    S2C_SNAPSHOT     = 0x0301,
+    C2S_SHOOT        = 0x0302,
+    C2S_USE_SKILL    = 0x0303,
+    S2C_HIT          = 0x0304,
+    S2C_SKILL_EFFECT = 0x0305,
+
+    S2C_GAME_OVER    = 0x0400,
+    C2S_GET_RANK     = 0x0401,
+    S2C_RANK_LIST    = 0x0402,
+
+    C2S_HEARTBEAT    = 0x0500,
+    S2C_HEARTBEAT    = 0x0501,
+    S2C_ERROR        = 0x0FFF,
+};
+
+struct MsgHeader {
+    MsgID    id;
+    uint16_t bodyLen;
+};
+
+struct Vec2 {
+    float x;
+    float y;
+};
+
+enum class TankType : uint8_t {
+    HEAVY = 0,
+    LIGHT = 1,
+    SCOUT = 2,
+};
+
+enum class RoomState : uint8_t {
+    WAITING = 0,
+    READY   = 1,
+    PLAYING = 2,
+};
+
+enum class SkillType : uint8_t {
+    SHIELD  = 0,
+    SPRINT  = 1,
+    SCATTER = 2,
+};
+
+struct TankState {
+    Vec2     pos;
+    float    angle;
+    int      curHP;
+    int      maxHP;
+    float    skillCooldown;
+    float    skillTimer;
+    TankType type;
+    int      playerID;
+    bool     alive;
+    bool     shieldActive;
+    bool     sprintActive;
+};
+
+struct BulletState {
+    Vec2 pos;
+    Vec2 vel;
+    int  owner;
+    int  damage;
+};
+
+struct SkillEffectState {
+    int       playerID;
+    SkillType type;
+    float     timer;
+};
+
+struct Snapshot {
+    uint32_t          frameSeq;
+    TankState         tanks[2];
+    BulletState       bullets[64];
+    int               bulletCount;
+    SkillEffectState  skills[2];
+};
+
+struct InputState {
+    bool w, a, s, d;
+    bool space;
+    bool f;
+};
+
+enum class ErrorCode : uint16_t {
+    NONE             = 0x0000,
+    LOGIN_FAILED     = 0x0001,
+    REGISTER_EXISTS  = 0x0002,
+    ROOM_FULL        = 0x0003,
+    INVALID_MSG      = 0x0004,
+    KICKED           = 0x0005,
+};
+
+constexpr uint16_t SERVER_PORT    = 9527;
+constexpr int      TICK_RATE      = 20;
+constexpr int      TICK_INTERVAL  = 1000 / TICK_RATE;
+constexpr int      RENDER_FPS    = 60;
+constexpr int      MAX_BULLETS   = 64;
+constexpr int      HEARTBEAT_MS  = 3000;
+constexpr int      RECONNECT_MS  = 10000;
